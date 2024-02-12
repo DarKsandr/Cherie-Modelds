6883e3cf128115f8ad3d7ffa4c462b35c6d358b5
volkov <aleksandr.volkov@ma.ru>
2024-02-12 18:08:38 +0300

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
