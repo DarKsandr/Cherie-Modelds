8758b9c28dd7d45de5975594acbdec29207e548b
volkov <aleksandr.volkov@ma.ru>
2024-02-12 21:00:50 +0300

    start news block

---
+++ components/index/news/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="h1">Новости</div>
+    <hr class="mt-10" />
+  </div>
+</template>
+
+<style scoped>
+.h1 {
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 32px;
+  letter-spacing: -0.04em;
+  text-align: left;
+}
+</style>
